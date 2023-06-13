cfb7c7a236669a16ae13206839e44e3a8bbe6efb
Kamrul Hasan Ridoy <ridoyhasan87@gmail.com>
2023-06-13 12:56:37 +0600

    JWT

--- index.js
+++ index.js
@@ -3,11 +3,29 @@ const app = express();
 const cors = require('cors');
 require('dotenv').config()
 
+const jwt = require('jsonwebtoken');
+
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
+const verifyJWT = (req,res,next)=>{
+  const authorization = req.headers.authorization;
+  if(!authorization){
+    return res.status(401).send({error: true, message: 'unauthorized access'});
+  }
+
+  const token = authorization.split(' ')[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET,(err, decoded)=>{
+    if(err){
+      return res.status(401).send({error: true, message: 'unauthorized access'})
+    }
+    req.decoded = decoded;
+    next();
+  })
+}
 
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
@@ -32,6 +50,12 @@ async function run() {
     const classCollection = client.db("schoolDB").collection("class");
     // const instructorCollection = client.db("schoolDB").collection("instructor");
     const cartCollection = client.db("schoolDB").collection("cart");
+
+    app.post('/jwt', (req,res)=>{
+      const user = req.body;
+      const token = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET, { expiresIn:'1h'})
+      res.send({token}) 
+    })
 
     app.get('/class',async(req,res)=>{
         const result = await classCollection.find().toArray();
