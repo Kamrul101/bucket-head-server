1ce08729c304bd7a5789f3f1ceb1fcdfc9963cc9
Kamrul Hasan Ridoy <ridoyhasan87@gmail.com>
2023-06-14 21:22:55 +0600

    payment

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 require('dotenv').config()
-
+const stripe = require('stripe')(process.env.PAYMENT_SECRET)
 const jwt = require('jsonwebtoken');
 
 const port = process.env.PORT || 5000;
@@ -161,6 +161,8 @@ async function run() {
       });
 
 
+
+
     app.post('/class',async(req,res)=>{
       const newItem = req.body;
       const result = await classCollection.insertOne(newItem);
@@ -185,6 +187,22 @@ async function run() {
         const query = {_id: new ObjectId(id)}
         const result = await classCollection.deleteOne(query);
         res.send(result);
+      })
+
+      //payment
+      app.post('/create-payment-intent',async(req,res)=>{
+        const {price}= req.body;
+        const amount = price *100;
+        const paymentIntent = await stripe.paymentIntents.create({
+          amount: amount,
+          currency:'usd',
+          payment_method_types:['card']
+        });
+        res.send(
+          {
+            clientSecret: paymentIntent.client_secret,
+          }
+        )
       })
   
     
